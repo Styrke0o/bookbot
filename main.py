1243dfacbfd3a3ccf9496684f3fdf8459ce94ccf
StyrkeOo <peter.mw.phu@gmail.com>
2025-08-19 21:31:35 +0100

    defined get_book_text() and programmed to print text to console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text():
+	with open('books/frankenstein.txt') as f:
+		file_contents = f.read()
+		return file_contents
+
+def main():
+	text=get_book_text()
+	print(text)
+
+main()
